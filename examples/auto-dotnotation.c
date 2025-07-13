56896ee66b640c601a972c389b3b6c43ff8fb59c
Udo <udo.schroeter@gmail.com>
2025-07-13 13:55:51 +0200

    auto dot

--- examples/auto-dotnotation.c
+++ examples/auto-dotnotation.c
@@ -8,17 +8,19 @@ struct PointList {
 };
 
 int main() {
-    PointList* p = (PointList*)malloc(sizeof(PointList));
+    PointList p0 = {y : 22};
+    PointList* p1 = (PointList*)malloc(sizeof(PointList));
     PointList* p2 = (PointList*)malloc(sizeof(PointList));
-    p->next = p2;
+    p1->next = p2;
 
-    p.x = 10;
-    p.y = 20;
+    p1.x = 10;
+    p1.y = 20;
     p2.x = 30;
     p2.y = 40;
 
-    printf("Point 1: (%d, %d)\n", p.x, p.y);
-    printf("Point 2: (%d, %d)\n", p.next.x, p.next.y);
+    printf("Point 0: (%d, %d, %p)\n", p0.x, p0.y, (void*)&p0);
+    printf("Point 1: (%d, %d, %p)\n", p1.x, p1.y, (void*)&p1);
+    printf("Point 2: (%d, %d, %p)\n", p1.next.x, p1.next.y, (void*)&p2);
     
     return 0;
 }
